39bc6018f37dd5fec1fc4c24062d00f134607a79
Nayara Dellamura <168658191+nayaradellamura@users.noreply.github.com>
2025-04-10 19:32:29 -0300

    commit

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
-  <title>HeathCare</title>
+  <title>HealthCare</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
 </head>
